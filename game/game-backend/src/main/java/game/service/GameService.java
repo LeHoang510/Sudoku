e9e94e2bf4f6592809afc6f1768813cd3ccc8191
le-hoang.nguyen <lehoangnguyen510@gmail.com>
2022-12-01 00:21:28 +0100

    update backend2

--- game/game-backend/src/main/java/game/service/GameService.java
+++ game/game-backend/src/main/java/game/service/GameService.java
@@ -34,26 +34,18 @@ public class GameService {
     public List<Grid> getGrids() {
         final List<Grid> grids = new ArrayList<>();
         try {
-            System.out.println("-get easy");
             final Grid easy = mapper.readValue(getGridFile("easy"), Grid.class);
-            System.out.println("-get medium");
             final Grid medium = mapper.readValue(getGridFile("medium"), Grid.class);
-            System.out.println("-get hard");
             final Grid hard = mapper.readValue(getGridFile("hard"), Grid.class);
-            System.out.println("-get veryhard");
             final Grid veryhard = mapper.readValue(getGridFile("veryhard"), Grid.class);
-            System.out.println("-get insane");
             final Grid insane = mapper.readValue(getGridFile("insane"), Grid.class);
-            System.out.println("-get inhuman");
             final Grid inhuman = mapper.readValue(getGridFile("inhuman"), Grid.class);
-            System.out.println("=>add to a list");
             grids.add(easy);
             grids.add(medium);
             grids.add(hard);
             grids.add(veryhard);
             grids.add(insane);
             grids.add(inhuman);
-            System.out.println("=>return the list\n\n\n\n");
             return grids;
         } catch (RuntimeException e) {
             e.printStackTrace();
@@ -66,9 +58,7 @@ public class GameService {
 
     public List<Score> getLeaderboard(final String level) {
         try {
-            final List<Score> leaderboard = Arrays.asList(mapper.readValue(getLeaderboardFile(level), Score[].class));
-            System.out.println("=>return the list\n\n\n\n");
-            return leaderboard;
+            return Arrays.asList(mapper.readValue(getLeaderboardFile(level), Score[].class));
         } catch (RuntimeException e) {
             e.printStackTrace();
             return null;
@@ -80,29 +70,29 @@ public class GameService {
 
     public Score addScore(final String level, final Score score) {
         try {
-            System.out.println("=>get leaderboard");
             final Score[] leaderboard = mapper.readValue(getLeaderboardFile(level), Score[].class);
-            System.out.println("=>adding score");
             final int length = leaderboard.length;
+            for (Score value : leaderboard) {
+                if (score.getName().equals(value.getName())) {
+                    if (score.getScore() <= value.getScore()) {
+                        value.setScore(score.getScore());
+                        writer.writeValue(getLeaderboardFile(level), leaderboard);
+                    }
+                    return score;
+                }
+            }
             if (length < 5) {
                 final Score[] scores = new Score[length + 1];
                 System.arraycopy(leaderboard, 0, scores, 0, length);
                 scores[length] = score;
-                System.out.println("=>update leaderboard");
                 writer.writeValue(getLeaderboardFile(level), scores);
-                System.out.println("=>success");
             } else {
                 Arrays.sort(leaderboard);
                 if (score.getScore() < leaderboard[0].getScore()) {
                     leaderboard[0] = score;
-                    System.out.println("=>update leaderboard");
                     writer.writeValue(getLeaderboardFile(level), leaderboard);
-                    System.out.println("=>success");
-                } else {
-                    System.out.println("=>fail");
                 }
             }
-            System.out.println("=>return score\n\n\n\n");
             return score;
         } catch (RuntimeException e) {
             e.printStackTrace();
